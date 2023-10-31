0c821ac8ad1361bb952200e1585d5c51d7061c51
kamwis003 <kamwis003@pbs.edu.pl>
2023-10-31 20:37:27 +0100

    v.04

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ function App() {
 
   function startWebSocket() {
 
-    ws1 = new WebSocket('wss://sig2.onrender.com');
+    ws1 = new WebSocket('wss://gis-395a905fe597.herokuapp.com');
     // let socketAddr = process.env.REACT_APP_SIGNALLING_SERVER;
     // ws1 = new WebSocket(socketAddr);
     ws1.onopen = event => {
